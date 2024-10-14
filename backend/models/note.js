0b1f878569f1cdc7bd89432635055179ec655d17
Carlos Natanauan <carlosbenedictn@gmail.com>
2024-10-14 20:41:09 +0800

    fix some minonr on noteRoute

--- backend/models/note.js
+++ backend/models/note.js
@@ -2,10 +2,11 @@
 const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
+  title: { type: String, required: false },  // Title is no longer required
+  content: { type: String, required: true },  // Content is still required
   createdAt: { type: Date, default: Date.now },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // Reference to the User
 });
+
 
 module.exports = mongoose.model('Note', noteSchema);
